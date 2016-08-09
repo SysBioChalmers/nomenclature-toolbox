d74691493c4a8ef31ec46e99a6ab0e67fbfe08c6
Daniel Hermansson <hedani@chalmers.se>
2016-08-09 13:24:05 +0200

    Update comments.

--- mapping/findMapping.m
+++ mapping/findMapping.m
@@ -1,4 +1,34 @@
 function [geneIds,geneMaps]=findMapping(aliases,targetDB,organismName)
+	% findMapping
+	% 	Finds a mapping for a set of protein IDs of a certain database
+	%	to a target database using the uniprot mapping service. The 
+	%	input alias types are identified based on regex patterns present
+	%	in the associative array 'uniprotIdMap' in the same folder.
+	%
+	%	Required: python bioservices should be installed and be
+	%	accessible from within MATLAB via 'py.bioservices'.
+	%	
+	%	input:
+	%	  aliases					a cell array of cell arrays of input
+	%								IDs (can be multiple different db ids
+	%								for each protein)
+	%	  targetDB					a database identifier string present 
+	%								in 'uniprotIdMap', (the end IDs will
+	%								be of this kind)
+	%	  organismName (opt)		a string with organism name to use
+	%								as backup search in ambiguous mappings,
+	%								this only applies if the proteins are
+	%								derived from the same organism.
+	%	output:
+	%	  geneIds					a cell array of converted IDs
+	%	  geneMaps					a cell of associative arrays of gene
+	%								IDs, can contain intermediate maps)
+	%	
+	%	usage:
+	%	  [geneIds,geneMaps]=findMapping(aliases,'UniProtKB AC','Escherichia coli')
+	%
+	% 	Daniel Hermansson, 01-06-2016
+
 	load('uniprotIdMap')
 
 	if nargin<3
@@ -9,10 +39,23 @@ function [geneIds,geneMaps]=findMapping(aliases,targetDB,organismName)
 		targetDB='UniProtKB AC';
 	end
 	
+	% The below functions are defined to enable effective use of 'cellfun'
+	% on the many intermediate cell arrays of the procedure.
+
+	% Chops an array at index k into length chunkSize.
 	chop=@(x,k,chunkSize) x(1+k*chunkSize:min((k+1)*chunkSize,length(x)));
+	
+	% Chops an array into equally sized chunks.
 	chopCell=@(x,chunkSize) arrayfun(@(k) chop(x,k,chunkSize),[1:floor(length(x)/chunkSize)],'UniformOutput',false);
+
+	% Helper search function to form correct search strings and convert the acquired
+	% python struct into MATLAB cells.
 	searchX=@(u,x,name) structFromPyObj(u.search(['(' strjoin(x,' OR ') ') organism:"' name '"'],'list'),false,true);
+	
+	% Small helper function to replicate a cell array.
 	rf=@(valCell,key) repmat({key},1,length(valCell));
+	
+	% Evaluates a mapping ID string mapId over a cell of regular expressions cellEx.
 	evalAccEx=@(cellEx,mapId) cellfun(@(x) regexp(x,mapId,'match'),cellEx,'UniformOutput',false,'ErrorHandler',@(S,varargin) []);
 
 	geneMaps={};
@@ -36,37 +79,45 @@ function [geneIds,geneMaps]=findMapping(aliases,targetDB,organismName)
 	% sort "id line" by ascending non-matches to find most present type of id among all gene aliases
 	[val,ind]=sort(cellfun(@(y) sum(cellfun(@(x) strcmp(class(x),'struct'),y)),nameCell));
 
-	% search for most present type (descending order)
+	% search for most present type (descending order) and use that alias to find a mapping to uniprot
+	% this will be the first map
 	for j=1:numel(ind)
 		aliasDbId=dbStr(regexKey{ind(j)}).id;
 		aliasSearch=nameCell{ind(j)};
 		map1=structFromPyObj(u.mapping(aliasDbId,dbStr('UniProtKB AC').id,aliasSearch'),false,true);
 		if(~isstruct(map1)) break; end
 	end
-
 	map1v=values(map1);
 
 	% if no uniprot returned, identify return, do a search based on organism name to verify
+	% this will produce a mapping with intermediate maps in the end (i.e. map1->map2->map3)
 	if (isempty(regexp(map1v{1},dbStr('UniProtKB AC').regex)) && isempty(regexp(map1v{1},dbStr(targetDB).regex)) && ischar(organismName))
 		
-		% identify each alias
+		% identify each alias (what kind of ID was returned)
+		% save most present one to aliasDbId
 		a=cellfun(@(x) evalAccEx(map1v,x),cellfun(@(x) dbStr(x).regex,regexKey,'UniformOutput',false),'UniformOutput',false);
 		b=cellfun(@(x) x(~cellfun('isempty',x)),a,'UniformOutput',false);
 		[i1,j1]=cellfun(@(x) size(x),b);
 		[i2,j2]=max(j1);
-
 		aliasDbId=dbStr(regexKey{ind(j2)}).id;
 
+		% intermediate map from aliasDB to standard Uniprot
 		map2=structFromPyObj(u.mapping(aliasDbId,dbStr('UniProtKB AC').id, map1v),false,true);
+
+		geneMaps={map2 map1};
+
+		% Note that this if clause handles cases when the above "u.mapping" not actually returns 
+		% standard uniprot, but something like UNIPARC IDs. We can then make a search based on the
+		% organism name instead to pick out relevant IDs and then create reverse maps to map back to 
+		% uniprot ids.
 
 		srch=values(map2);
 		srchIn=[srch{:}];
 
+		% send searches with 100 IDs in each search, note that this uses u.search() not u.mapping()
 		gensrch=cellfun(@(srch) searchX(u,srch,organismName),chopCell(srchIn,100),'UniformOutput',false);
 
-		geneMaps={map2 map1};
-
-		% check for other organism name
+		% if search is empty check if reason is that the organism name has a synonym in the database which could be used instead
 		if isempty([gensrch{:}])
 			name=strread(structFromPyObj(u.search(pyargs('query',organismName,'limit',1,'sort','score','frmt','xls','columns','organism'))), '%[^\n]',2);
 			if(isempty(name))
@@ -76,6 +127,7 @@ function [geneIds,geneMaps]=findMapping(aliases,targetDB,organismName)
 			gensrch=cellfun(@(srch) searchX(u,srch,name{2}),chopCell(srchIn,100),'UniformOutput',false);
 		end
 
+		% some restructuring of the search cell and intermediate maps tmp2 for the steps below
 		gensrch=cellfun(@(x) strsplit(x,'\n'),gensrch,'UniformOutput',false);
 		gensrch=[gensrch{:}];
 		gensrch(strcmp('', gensrch))=[];
@@ -89,21 +141,27 @@ function [geneIds,geneMaps]=findMapping(aliases,targetDB,organismName)
 			end
 		end
 
+		% since some of the matches are partial, the values in the reverse map tmp2r
+		% have to be duplicated in order to make a one to one map
 		tmp2kRep=cellfun(@(x,y) rf(x,y),tmp2v',tmp2k','UniformOutput',false);
 		tmp2kRep=[tmp2kRep{:}];
 
 		tmp2r=containers.Map([tmp2v{:}],tmp2kRep);
 
+		% the reverse map is applied to pick out only those IDs which were 
+		% initally present
 		srchE=applyMap({tmp2r},gensrch);
-
 		tmp3=containers.Map(srchE,gensrch);
 
+		% the final map is applied to yield uniprot IDs
 		geneIds=applyMap({tmp3},values(map1))';
-	else
+	else % no intermediate maps need to be produced (just return map1)
 		geneMaps={map1};
 		geneIds=applyMap(geneMaps,aliasSearch);
 	end
 
+	% if targetDB is not uniprot perform the last search with the acquired uniprot IDs
+	% to produce map3 and the final targetDB geneIds
 	if(isempty(regexp(targetDB,'UniProtKB')))
 		map3=structFromPyObj(u.mapping(dbStr('UniProtKB AC').id,dbStr(targetDB).id,geneIds'),false,true);
 		geneIds=applyMap({map3},geneIds);
--- other/applyMap.m
+++ other/applyMap.m
@@ -1,36 +1,35 @@
-function out = applyMap(mapsIn,out)%,returnOld)
+function str = applyMap(mapsIn,str)
+	% applyMap
+	% 	Successively applies a set of maps (dictionaries) to a
+	%	set of strings. If any key value pairs do not exist, the
+	%	last applied mapping that worked is returned.
+	%
+	%	input:
+	%	  mapsIn					a cell array of dictionaries in application
+	%								order right to left {map3,map2,map1}
+	%	  str						the input strings on which to apply maps
+	%
+	%	output:
+	%	  str						a cell array of converted strings 
+	%	
+	%	usage:
+	%	  str = applyMap({map3,map2,map1},inputString)
+	%
+	% 	Daniel Hermansson, 01-06-2016
 	%global returnOld;
 
 	map=mapsIn{end};
-	%try
-	%	out=cell2mat(cellfun(@(x) mapTo(x,map),out,'UniformOutput',false,'ErrorHandler',@errorfun));
-	%catch
-	%	out=cellfun(@(x) mapTo(x,map),out,'UniformOutput',false,'ErrorHandler',@errorfun);
-	%end
+	
+	str=cellfun(@(x) mapTo(x,map),str,'UniformOutput',false,'ErrorHandler',@errorfun);
 
-	out=cellfun(@(x) mapTo(x,map),out,'UniformOutput',false,'ErrorHandler',@errorfun);
-
-	if(length(mapsIn)>1)% && strcmp(class(funs),'cell'))
-		out=applyMap(mapsIn(1:end-1),out);%,returnOld);
+	if(length(mapsIn)>1)
+		str=applyMap(mapsIn(1:end-1),str);
 	end	
 end
 
 function result = errorfun(S, varargin)
-	% supress error
-	%warning(S.identifier, S.message)
-	%disp(varargin)
-	%for i=1:length(varargin)
-	%	fprintf([num2str(i) ': '])
-	%	disp(varargin)
-	%end
-	
-	%if length(varargin)>1
-   	result = varargin{1};
-   	%else
-   	%	result = {};
-   	%end
+	result = varargin{1};
 end
-
 
 function res = mapTo(x,map)
 	errorStruct.message = 'Field value empty.';
